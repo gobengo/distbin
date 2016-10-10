a8601214980be122f7f1f262cc68576a7bc524e1
Benjamin Goering <bengoering@gmail.com>
2016-10-10 20:56:25 +0545

    npm test prints test name

--- test/index.js
+++ test/index.js
@@ -1,10 +1,24 @@
-exports['The tests pass'] = function () {
-	// always pass for now
+const assert = require('assert')
+const distbin = require('../')
+
+let tests = module.exports;
+
+tests['distbin can be imported'] = function () {
+	assert(distbin, 'distbin is truthy')
 }
 
 // Run tests if this file is executed
 if (require.main === module) {
-	Object.entries(exports).forEach(([testName, runTest]) => {
-		runTest();
+	let failed = false;
+	Object.entries(tests).forEach(([testName, runTest]) => {
+		try {
+			runTest();
+		} catch (e) {
+			failed = true;
+			console.error('TEST FAIL: ', testName, '\n', e.stack);
+		}
 	})
+	if (failed) {
+		process.exit(1);
+	}
 }
