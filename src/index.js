4fee2ab7f45d436317f6683212eebdbc3f2a9a32
Benjamin Goering <bengoering@gmail.com>
2016-10-11 00:53:26 +0545

    Can handle http.Requests

---
+++ src/index.js
@@ -0,0 +1,6 @@
+module.exports = function () {
+	return function (req, res) {
+		res.writeHead(200)
+		res.end()
+	}
+};
--- test/index.js
+++ test/index.js
@@ -1,24 +1,94 @@
 const assert = require('assert')
 const distbin = require('../')
+const http = require('http');
 
 let tests = module.exports;
 
-tests['distbin can be imported'] = function () {
+// given a handler function like (req, res), make it listen
+// then send http.request, return a Promise or response
+const sendRequest = (handler, request) => {
+	const server = http.createServer(handler);
+	let listened;
+	return new Promise((resolve, reject) => {
+		server
+			.once('error', () => {
+				if ( ! listened) reject();
+			})
+			.listen(0, () => {
+				listened = true;
+				resolve();
+			})
+	})
+	.then(() => new Promise((resolve, reject) => {
+		const url = `http://localhost:${server.address().port}`
+		const requestOptions = Object.assign({
+			hostname: 'localhost',
+			method: 'get',
+			path: '/',
+			port: server.address().port
+		}, request);
+		const req = http
+			.request(url, (res) => {
+			  res.destroy();
+			  resolve(res);
+			})
+			.on('error', reject)
+			.end();
+	}))
+}
+
+tests['distbin can be imported'] = () => {
 	assert(distbin, 'distbin is truthy')
+}
+
+tests['can create a distbin'] = () => {
+	const bin = distbin();
+}
+
+tests['can send http requests to a distbin.Server'] = () => {
+	return sendRequest(distbin(), { method: 'get' }).then((res) => {
+		assert.equal(res.statusCode, 200);
+	})
 }
 
 // Run tests if this file is executed
 if (require.main === module) {
 	let failed = false;
-	Object.entries(tests).forEach(([testName, runTest]) => {
-		try {
-			runTest();
-		} catch (e) {
-			failed = true;
-			console.error('TEST FAIL: ', testName, '\n', e.stack);
-		}
+	Promise.all(
+		// map to array of promises of logged errors
+		// (or falsy if the test passed)
+		Object.entries(tests).map(([testName, runTest]) => {
+			function logFailure(err) {
+				console.error(`TEST FAIL: ${testName}\n${err.stack}\n`)
+			}
+			let result;
+			try {
+				result = runTest();
+			} catch (err) {
+				logFailure(err);
+				return err;
+			}
+			// result allowed to be a promise
+			return Promise.resolve(result)
+			.then(() => {}) // return nothing if success
+			.catch(err => {
+				failed = true;
+				logFailure(err);
+				return err;
+				// throw err;
+			})
+		})
+	).catch((err) => {
+		console.error("ERROR IN TEST RUNNER", err)
+		process.exit(1)
 	})
-	if (failed) {
-		process.exit(1);
-	}
+	.then((results) => {
+		const failures = results.filter(Boolean);
+		if (failures.length) {
+			console.error(`${failures.length} test failures`);
+			process.exit(1);
+			return;
+		}
+		process.exit();
+	});
 }
