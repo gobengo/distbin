42d7903479766728991519e527b74f5fc5c5902a
Benjamin Goering <bengoering@gmail.com>
2016-10-11 13:13:40 +0545

    Give structure to tests

--- test/index.js
+++ test/index.js
@@ -4,38 +4,6 @@ const http = require('http')
 
 let tests = module.exports
 
-// given a handler function like (req, res), make it listen
-// then send http.request, return a Promise or response
-const sendRequest = async function(handler, request) {
-  const server = http.createServer(handler)
-  let listened
-  await new Promise((resolve, reject) => {
-    server
-    .once('error', () => {
-      if (!listened) reject()
-    })
-    .listen(0, () => {
-      listened = true
-      resolve()
-    })
-  })
-  return new Promise((resolve, reject) => {
-    const requestOptions = Object.assign({
-      hostname: 'localhost',
-      method: 'get',
-      path: '/',
-      port: server.address().port
-    }, typeof(request) === 'string' ? { path: request } : request)
-    http
-      .request(requestOptions, (res) => {
-        res.destroy()
-        resolve(res)
-      })
-      .on('error', reject)
-      .end()
-  })
-}
-
 tests['distbin can be imported'] = () => {
   assert(distbin, 'distbin is truthy')
 }
@@ -49,14 +17,17 @@ tests['can send http requests to a distbin.Server'] = async function() {
   assert.equal(res.statusCode, 200)
 }
 
+// 5.6 Public Addressing - https://w3c.github.io/activitypub/#public-addressing
 tests['can request the public Collection'] = async function () {
   const res = await sendRequest(distbin(), '/public')
   assert.equal(res.statusCode, 200);
 }
 
-// tests['can post a Note'] = async function() {
-//   const res = await sendRequest(distbin(), { method: 'get' })
-// }
+// 6 Binary Data - #TODO
+
+// 7 Client to Server Interactions - https://w3c.github.io/activitypub/#client-to-server-interactions
+
+// 7.1 Create Activity - https://w3c.github.io/activitypub/#create-activity-outbox
 
 // Run tests if this file is executed
 if (require.main === module) {
@@ -96,4 +67,36 @@ async function run(tests) {
   if (failures.length) {
     console.error(`${failures.length} test failures`)
   }
+}
+
+// given a node.http handler function accepting (req, res), make it listen
+// then send an http.request, return a Promise of response
+async function sendRequest(handler, request) {
+  const server = http.createServer(handler)
+  let listened
+  await new Promise((resolve, reject) => {
+    server
+    .once('error', () => {
+      if (!listened) reject()
+    })
+    .listen(0, () => {
+      listened = true
+      resolve()
+    })
+  })
+  return new Promise((resolve, reject) => {
+    const requestOptions = Object.assign({
+      hostname: 'localhost',
+      method: 'get',
+      path: '/',
+      port: server.address().port
+    }, typeof(request) === 'string' ? { path: request } : request)
+    http
+      .request(requestOptions, (res) => {
+        res.destroy()
+        resolve(res)
+      })
+      .on('error', reject)
+      .end()
+  })
 }
