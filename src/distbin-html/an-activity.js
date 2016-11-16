bf81a5cc53c6943ce53d346d6c446e8bae67b715
Benjamin Goering <bengoering@gmail.com>
2016-11-16 17:55:06 +0700

    indented styling for ancestor chain

--- src/distbin-html/an-activity.js
+++ src/distbin-html/an-activity.js
@@ -18,21 +18,34 @@ exports.createHandler = ({apiUrl, activityId}) => {
     }
     const activity = JSON.parse(await readableToString(activityRes))
     const ancestors = await fetchReplyAncestors(activity)
+    // Render a single ancestor activity
     const renderAncestor = (ancestor) => {
       if (ancestor[failedToFetch]) {
         // assume its a broken link
         return `
-          <article>
+          <article class="activity-item">
             <a href="${ancestor.href}">${ancestor.href}</a> (couldn't fetch more info)
           </article>
         `
       }
       return `
-        <article>
+        <article class="activity-item">
           <a href="${ancestor.url}">
             <main>${encodeHtmlEntities(ancestor.object.content)}</main>
           </a>
         </article>
+      `
+    }
+
+    // Render an item and its ancestors for each ancestor in the array.
+    // This results in a nested structure conducive to indent-styling
+    const renderAncestorsSection = (ancestors) => {
+      const [ancestor, ...olderAncestors] = ancestors;
+      return `
+        <div class="ancestors">
+          ${olderAncestors.length ? renderAncestorsSection(olderAncestors) : ''}
+          ${renderAncestor(ancestor)}
+        </div>
       `
     }
 
@@ -41,24 +54,26 @@ exports.createHandler = ({apiUrl, activityId}) => {
       <!doctype html>
       <head>
         ${everyPageHead()}
+        <style>
+        .ancestors {
+          border-left: 1px solid #ddd;
+          padding-left: 1em;
+        }
+        .activity-item main {
+          margin: 1em auto; /* intended to be same as <p> to force same margins even if main content is not a p */
+        }
+        </style>
       </head>
 
-      ${ancestors.length
-        ? `
-          ${ancestors.reverse().map(renderAncestor).join('<hr />')}
-          <hr />
-          `
-        : ''
-      }
+      ${renderAncestorsSection(ancestors)}
 
-
-      <article>
+      <article class="activity-item">
         ${activity.object.name
           ? `<h1>${activity.object.name}</h1>`
           : ''}
         <main>${encodeHtmlEntities(activity.object.content)}</main>
         ${/* TODO format published datetime, add byline */''}
-        <p>at ${activity.published}</p>
+        <footer>at ${activity.published}</footer>
       </article>
       <details>
         <summary>Raw</summary>
